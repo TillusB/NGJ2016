6f8ea53faf6d34ca22278e75c28f3ebed76359f8
LocalDude <christian91_24@hotmail.com>
2016-04-09 20:02:18 +0200

    Testing

---
+++ Assets/scripts/Obsticles/Others/TileSpriteManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileSpriteManager : MonoBehaviour {
+
+	public static TileSpriteManager ourInstance = null;
+
+	public List<TileSpriteSwapper> spriteSwapers = new List<TileSpriteSwapper>();
+
+	public static TileSpriteManager GetInstance()
+	{
+		return ourInstance;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		if(ourInstance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		ourInstance = this;
+	}
+
+	public void UpdateSprites(int worldState)
+	{
+		for(int index = 0; index < spriteSwapers.Count; index++)
+		{
+			spriteSwapers[index].StartSwapTo(worldState);
+		}
+	}
+}
---
+++ Assets/scripts/Obsticles/Others/TileSpriteSwapper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileSpriteSwapper : MonoBehaviour {
+
+	public List<Sprite> stageSprites;
+	bool isSwapping = false;
+	public SpriteRenderer renderer;
+	public SpriteFader fader;
+
+	public int currentState = 5;
+
+	// Use this for initialization
+	void Start () {
+		if(!isSwapping)
+			enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartSwapTo(int bunnyState)
+	{
+		bunnyState = Mathf.Max(1, bunnyState);
+		if(currentState != bunnyState)
+		{
+			fader.FadeFrom(stageSprites[currentState]);
+			renderer.sprite = stageSprites[bunnyState];
+			isSwapping = true;
+			enabled = true;
+		}
+	}
+}
