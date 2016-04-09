14b5c9eef0db258f31a9f617664623d6468be581
Till Balbach <tbalbach@gmx.net>
2016-04-09 13:02:28 +0200

    generic pickup item and itemmanager

---
+++ Assets/scripts/PickupItems/Item.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public ItemManager.Effect itemEffect = ItemManager.Effect.NONE;
+    public float amount = 0;
+
+    public void OnEnable()
+    {
+        ItemManager.GetInstance().AddItem(this);
+    }
+
+    public void OnDissable()
+    {
+        ItemManager.GetInstance().RemoveItem(this);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+        {
+            ItemManager.GetInstance().ApplyEffect(this);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+        {
+            ItemManager.GetInstance().ApplyEffect(this);
+        }
+        
+    }
+
+}
---
+++ Assets/scripts/PickupItems/ItemManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemManager : MonoBehaviour {
+    public enum Effect
+    {
+        NONE,
+        HEAL,
+        SPEED,
+        REDUCEBLEED
+    }
+
+    static ItemManager ourInstance;
+
+    public GameObject player;
+    public Bunny bunny;
+
+    static public ItemManager GetInstance()
+    {
+        return ourInstance;
+    }
+
+    public List<Item> items = new List<Item>();
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (ourInstance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+            ourInstance = this;
+        player = GameObject.Find("Bunny");
+    }
+
+    public void AddItem(Item item)
+    {
+        if (!items.Contains(item))
+        {
+            items.Add(item);
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public void ApplyEffect(Item item)
+    {
+        if(item.itemEffect == Effect.HEAL)
+        {
+            bunny.increaseHealth(item.amount);
+        }
+        if(item.itemEffect == Effect.SPEED)
+        {
+            bunny.setSpeed(item.amount);
+        }
+        if(item.itemEffect == Effect.REDUCEBLEED)
+        {
+            // TODO: reducebleed in the gamemmanager for a certain amount of time?
+        }
+    }
+
+}
