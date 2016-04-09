4118a38e99e7894e520ac554c5ea3cb2312c9bbc
LocalDude <christian91_24@hotmail.com>
2016-04-09 10:39:04 +0200

    Chj - Added killer areas
    so we can kill bunny.... not working yet

---
+++ Assets/scripts/Obsticles/KillerArea.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerArea : MonoBehaviour {
+
+	public float enterDamage = 5;
+	public float stayDamage = 0;
+	public float exitDamage = 0;
+
+	public ExpellDirection expellType = ExpellDirection.NONE;
+
+	public Vector3 expellPoint;
+	
+
+	public enum ExpellDirection
+	{
+		NONE,
+		OMNI,
+		DIRECTION,
+		//PLANE,
+	}
+
+	public void OnEnable()
+	{ 
+		KillerAreaManager.GetInstance().AddKillerArea(this);
+	}
+
+	public void OnDissable()
+	{
+		KillerAreaManager.GetInstance().RemoveKillerArea(this);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+		{
+			KillerAreaManager.GetInstance().DamagePlayer(exitDamage);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+		{
+			KillerAreaManager.GetInstance().DamagePlayer(enterDamage);
+		}
+		ExpellPlayer(other.transform);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+		{
+			KillerAreaManager.GetInstance().DamagePlayer(enterDamage);
+		}
+		ExpellPlayer(other.transform);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.transform.IsChildOf(KillerAreaManager.GetInstance().gameObject.transform))
+		{
+			KillerAreaManager.GetInstance().DamagePlayer(stayDamage * Time.deltaTime);
+		}
+		ExpellPlayer(other.transform);
+	}
+
+	public void ExpellPlayer(Transform playerPoint)
+	{
+		if(expellType == ExpellDirection.DIRECTION)
+		{
+			//Player.ApplyForce(expellPoint);
+		}
+		else if(expellType == ExpellDirection.OMNI)
+		{
+			//Player.ApplyForce((expellPoint - playerPoint.position).Normalized);
+		}
+	}
+}
---
+++ Assets/scripts/Obsticles/KillerAreaManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KillerAreaManager : MonoBehaviour {
+
+	static KillerAreaManager ourInstance;
+
+	public GameObject player;
+
+	static public KillerAreaManager GetInstance()
+	{
+		return ourInstance;
+	}
+
+	public List<KillerArea> areas = new List<KillerArea>();
+
+	// Use this for initialization
+	void Awake () {
+		if(ourInstance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+			ourInstance = this;
+	}
+
+	public void DamagePlayer(float damage)
+	{
+		
+	}
+
+	public void AddKillerArea(KillerArea area)
+	{
+		if(!areas.Contains(area))
+		{
+			areas.Add(area);
+		}
+	}
+
+	public void RemoveKillerArea(KillerArea area)
+	{
+		areas.Remove(area);
+	}
+}
