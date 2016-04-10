41d3a61496d91efbe9e975cf6330331ba6fcef25
LocalDude <christian91_24@hotmail.com>
2016-04-10 12:11:15 +0200

    Chj - tint

---
+++ Assets/scripts/Obsticles/Tinter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tinter : MonoBehaviour {
+
+	public Gradient colors;
+
+	public MeshRenderer renderer;
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.color = colors.Evaluate(KillerAreaManager.GetInstance().bunny.getHealth() / 100.0f);
+	}
+}
